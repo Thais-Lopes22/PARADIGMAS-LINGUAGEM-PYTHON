82c5630d12a5931ddd01f719abe9461c2fd5df14
Thais Lopes <thaislopes0887@gmail.com>
2026-09-10 16:22:40 -0300

    atualizando o repositorio

---
+++ QUESTAO01.py
@@ -0,0 +1,8 @@
+nome = 'Juvenaldo Florentino'
+nome = "".join(nome.split())
+
+# ' = serve para delimitar uma string.
+# . = acessa metodos ou atributos de um objeto.
+# "" = string vazia, usada como separador no join().
+# split = separa a string em partes, usando os espacos como separadores.
+# join = junta novamente os elementos da lista em uma unica string.
