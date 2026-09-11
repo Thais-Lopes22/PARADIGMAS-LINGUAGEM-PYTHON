cbf815f55fb2680a006803b11955c9fa235e9d66
Thais Lopes <thaislopes0887@gmail.com>
2026-09-10 23:18:23 -0300

    atualizando questao 4

---
+++ QUESTAO04.py
@@ -0,0 +1,8 @@
+letras = "ABDE"
+print(letras + "FGH")
+
+# letras = variável que contém "ABDE".
+# "ABDE" = primeira string.
+# + = operador usado para concatenar/juntar strings.
+# "FGH" = segunda string.
+# Resultado: ABDEFGH
