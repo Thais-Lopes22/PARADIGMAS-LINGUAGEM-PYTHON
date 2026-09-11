100fce65569f6329833f4d8e02cb431075ee34fb
Thais Lopes <thaislopes0887@gmail.com>
2026-09-10 23:13:47 -0300

    atualizando questao 3

---
+++ QUESTAO03.py
@@ -0,0 +1,7 @@
+alfabeto = "abcdefghijklmnopqrstuvwxyz"
+print(alfabeto[0])
+
+# alfabeto = variável que armazena o alfabeto.
+# [0] = acessa um caractere através do seu índice.
+# 0 = representa o primeiro índice da string, pois em Python a contagem começa em zero.
+# Resultado: a
